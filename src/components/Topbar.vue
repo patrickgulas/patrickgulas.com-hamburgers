0318dc7b65818f9ba7f12f0500baef9f2d1cca37
Patryk Gułaś <patryk.gulas@digifit.pl>
2020-11-23 23:00:46 +0100

    init commit

---
+++ src/components/Topbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-gray-100">
+    <div class="container">
+      <div class=" px-4 py-6 text-center font-bold flex flex-col">
+        Tailwind Hamburgers
+        <a
+          class="self-center mt-2 text-gray-600 hover:text-gray-700"
+          href="https://github.com/patrickgulas/tailwind-hamburgers"
+          target="_blank"
+        >
+          <GithubIcon class="h-6 w-auto" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import GithubIcon from './icons/GithubIcon.vue'
+
+export default Vue.extend({
+  components: {
+    GithubIcon,
+  },
+})
+</script>
---
+++ src/layouts/Default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Topbar />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import Topbar from '../components/Topbar.vue'
+
+export default Vue.extend({
+  components: {
+    Topbar,
+  },
+})
+</script>
